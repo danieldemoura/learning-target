8eb4f853c1d6d37d381548ca36bc0ab5ce439f70
Daniel de Moura <nazuyamikima@gmail.com>
2024-05-23 17:20:02 -0300

    alterado a key

--- src/api/fetchData.js
+++ src/api/fetchData.js
@@ -1,6 +1,6 @@
 export const fetchData = async (endpoint = "") => {
     try {
-        const apiKey = 'AIzaSyABXWnWABmfp0XrrnvftNQgFbOkD4zyNqU'
+        const apiKey = 'AIzaSyCkzL9l-0tS3SXxMWUErD08FlFHpTk07RM'
         const url = `https://www.googleapis.com/youtube/v3/${endpoint}&key=${apiKey}`
 
         const response = await fetch(url)
